39ea5070c7205994363c56531723243566587c4b
D1ksan <ekhestt@gmail.com>
2024-10-15 16:34:03 +0300

    complete: montly Payment of credit

---
+++ laba2_4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double stoim_kv, nach_vz, proc_st, sum_kredit;
+    int years;
+    printf("Введите стоимость квартиры:");
+    scanf("%lf",&stoim_kv);
+    printf("Введите начальный взнос:");
+    scanf("%lf",&nach_vz);
+    printf("Введите число лет ипотеки:");
+    scanf("%d",&years);
+    printf("Введите процентную ставку:");
+    scanf("%lf",&proc_st);
+    
+    sum_kredit = stoim_kv - nach_vz;
+
+    double monthlyRate = (proc_st / 100) / 12;
+    int total = years * 12;
+    
+    //по формуле аннуитета
+    double monthlyPayment = (sum_kredit * monthlyRate) / 
+                            (1 - pow(1 + monthlyRate, -total));
+    
+    double totalPaid = monthlyPayment * total;
+    
+    printf("Ежемесячный платеж: %.2f\n", monthlyPayment);
+    printf("Общая сумма выплат: %.2f\n", totalPaid);
+    
+    printf("Платежи по годам:\n");
+    
+    for (int year = 1; year <= years; year++) {
+        double yearlyPayment = monthlyPayment * 12; // Годовой платеж
+        printf("Год %d: %.2f\n", year, yearlyPayment);
+    }
+
+    return 0;
+}
+    
+
+
+
